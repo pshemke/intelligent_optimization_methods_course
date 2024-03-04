c36796c54041721debedb21486f5e2b80ed6ed3a
pshemke <67806185+pshemke@users.noreply.github.com>
2024-03-04 20:05:23 +0100

    moving between computers

--- lab01/main.cpp
+++ lab01/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <bitset>
+#include <tuple>
 
 int main() {
-    std::cout << "Hello World!";
+    std::string input_path = "";
+
+    std::bitset<100> visited;
+    std::bitset<100> edges[100];
+    int distances[100][100];
+    std::tuple<int,int> nodes[100];
+    
+    
+
     return 0;
 }
